e34b4b757b3220504f4b088073036139e5a90df8
talagabartosz <talaga.bartosz@gmail.com>
2015-04-25 20:56:17 +0200

    getElemntLoopTest method created

--- src/talagakrol/FibonaciLauncherTest.java
+++ src/talagakrol/FibonaciLauncherTest.java
@@ -1,5 +1,15 @@
 package talagakrol;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class FibonaciLauncherTest {
+
+	@Test
+	public void getElemntLoopTest() {
+		assertEquals(1, Fibonacci.getElemntLoop(1).intValue());
+		assertEquals(8, Fibonacci.getElemntLoop(6).intValue());
+		assertEquals(144, Fibonacci.getElemntLoop(12).intValue());
+	}
 
 }
