3fab78d0f39c3cad20b2daad88d290608fe1508a
talagabartosz <talaga.bartosz@gmail.com>
2015-04-25 20:49:44 +0200

    getElemntLoop method created

--- src/talagakrol/Fibonacci.java
+++ src/talagakrol/Fibonacci.java
@@ -1,5 +1,27 @@
 package talagakrol;
 
+import java.math.BigInteger;
+
 public class Fibonacci {
 
+	public static BigInteger getElemntLoop(int index) {
+
+		if (index < 1)
+			throw new IllegalArgumentException();
+		if (index == 1)
+			return new BigInteger("1");
+		if (index == 2)
+			return new BigInteger("1");
+
+		BigInteger a = new BigInteger("1");
+		BigInteger b = new BigInteger("1");
+		BigInteger c = new BigInteger("0");
+
+		for (int i = 3; i <= index; i++) {
+			c = b.add(a);
+			a = b;
+			b = c;
+		}
+		return c;
+	}
 }
