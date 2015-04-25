1b4d668ed06fee057240accb01710e3a2cf5e489
talagabartosz <talaga.bartosz@gmail.com>
2015-04-25 20:51:59 +0200

    Main method created.

--- src/talagakrol/FibonaciLuncher.java
+++ src/talagakrol/FibonaciLuncher.java
@@ -4,7 +4,13 @@ public class FibonaciLuncher {
 
 	public static void main(String[] args) {
 		
-
+		for (int i = 1;; i++) {
+			
+			if (Fibonacci.getElemntLoop(i).toString().length() == 1000) {
+				System.out.println(i);
+				System.out.println(Fibonacci.getElemntLoop(i));
+				return;
+			}
+		}
 	}
-
 }
